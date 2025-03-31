e9f49bdf16b9c9599f6eceb19094fe20878976c5
Elie <niyubwayoiraelie5777@gmail.com>
2025-03-31 23:46:38 +0200

    Building Product API

--- core/admin.py
+++ core/admin.py
@@ -5,6 +5,16 @@ from .models import CustomUser
 # Register your models here.
 
 class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    def get_fieldsets(self, request, obj=None):
+        return (
+            (None, {
+                'classes': ('wide',),
+                'fields': ('username', 'email', 'first_name', 'last_name', 'password1', 'password2', 'city', 'state', 'address', 'phone', 'is_staff', 'is_active')
+            }),
+        )
+
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
--- core/models.py
+++ core/models.py
@@ -3,11 +3,13 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-class CustomUser(models.Model):
+class CustomUser(AbstractUser):
     city = models.CharField(max_length=100, blank=True, null=True)
     state = models.CharField(max_length=100, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
     phone = models.CharField(max_length=15, blank=True, null=True)
+
+    REQUIRED_FIELDS = ['city', 'state', 'address', 'phone']
 
     def __str__(self):
         return self.username
