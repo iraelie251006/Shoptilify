ba4a2b51c3e5b097270c7db1ab81cfe2fb604c4e
Elie <niyubwayoiraelie5777@gmail.com>
2025-04-02 18:33:02 +0200

    Add Product Items and Bug Fix in ProductSerializer

--- shop_app/serializers.py
+++ shop_app/serializers.py
@@ -3,5 +3,5 @@ from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
-        model: Product
-        fields: ['id', 'name', 'slug', 'image', 'description', 'category', 'price']
+        model = Product
+        fields = ['id', 'name', 'slug', 'image', 'description', 'category', 'price']
