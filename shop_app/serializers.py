dc7dc9a4bb8a389700a417b2319c628b8dbd1715
Elie <niyubwayoiraelie5777@gmail.com>
2025-04-04 23:31:43 +0200

    Added detailed product serializer and view for product detail endpoint.

--- shop_app/serializers.py
+++ shop_app/serializers.py
@@ -5,3 +5,15 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['id', 'name', 'slug', 'image', 'description', 'category', 'price']
+
+class DetailedProductSerializer(serializers.ModelSerializer):
+    similar_products = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'slug', 'image', 'description', 'category', 'price', 'similar_products']
+
+    def get_similar_products(self, product):
+        products = Product.objects.filter(category=product.category).exclude(id=product.id)
+        serializer = ProductSerializer(products, many=True)
+        return serializer.data
